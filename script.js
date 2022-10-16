a814a56d1efd09877a78fe8160b18a5a46fd401d
hayoun <fishingest@kookmin.ac.kr>
2022-10-16 21:43:02 +0900

    비교 연산자

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-alert('Hello');
-alert('World');
+let a=1;
+var b=1;
+const c=1;
+
+// alert('Hello');
+// alert('World');
+
+// const userName = prompt("이름을 알려주세요.", "영희");
+// let isTeaWanted = confirm("차 한 잔 드릴까요?");
+
+alert( 0 == false ); // true
+alert( 0 == '' ); // true
+
+alert( 0 ===false);//false
